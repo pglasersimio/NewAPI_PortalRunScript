71d07c845da996ddc4f3e148df35c1c4190d28a7
pglasersimio <134318309+pglasersimio@users.noreply.github.com>
2025-02-03 14:35:43 -0500

    Updates to add control value and revised syntax for dates

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 
 echo "Installing required packages..."
 pip install load_dotenv tenacity requests decorator
-pip install -i https://test.pypi.org/simple/ pysimio>=0.0.9
+pip install -i https://test.pypi.org/simple/ pysimio>=0.0.10
 echo "All packages installed successfully."
